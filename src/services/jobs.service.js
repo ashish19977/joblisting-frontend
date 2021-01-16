6b9ca82a79da80b2765c18ee7eafe77c66d3fa10
Ashish Kumar <ashish.kumar@phablecare.com>
2021-01-16 12:03:50 +0530

    baseurl setup

--- src/services/jobs.service.js
+++ src/services/jobs.service.js
@@ -2,9 +2,9 @@
 const fetch = require('node-fetch')
 
 const getJobs = async ({ page = 1, pageSize = 24, searchKey = undefined}) => {
-  console.log(page, pageSize)
   try{
-    const data = await fetch(`http://localhost:8000?page=${page-1}&pageSize=${pageSize}&searchKey=${searchKey || ''}`)
+    const baseUrl = process.env.baseUrl
+    const data = await fetch(`${baseUrl}/?page=${page-1}&pageSize=${pageSize}&searchKey=${searchKey || ''}`)
     return await data.json()
   }catch(e){
     throw e
