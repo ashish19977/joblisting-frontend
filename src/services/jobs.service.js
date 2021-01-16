f2e6e32ef1f68bc6f59c743bd809d744758a225f
Ashish Kumar <ashish.kumar@phablecare.com>
2021-01-16 14:04:36 +0530

    Env setup

--- src/services/jobs.service.js
+++ src/services/jobs.service.js
@@ -1,9 +1,9 @@
 
 const fetch = require('node-fetch')
+const { baseUrl } = require('../Constants')
 
 const getJobs = async ({ page = 1, pageSize = 24, searchKey = undefined}) => {
   try{
-    const baseUrl = process.env.REACT_APP_baseUrl
     const data = await fetch(`${baseUrl}?page=${page-1}&pageSize=${pageSize}&searchKey=${searchKey || ''}`)
     return await data.json()
   }catch(e){
